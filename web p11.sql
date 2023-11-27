1971bebcdb6131eef82f6c78fe341133dc48582e
Tedysugiarta <97079363+Tedysugiarta@users.noreply.github.com>
2023-11-27 22:43:54 +0800

    Add files via upload

---
+++ web p11.sql
@@ -0,0 +1,12 @@
+`2101020011_tedysugiartaa``2101020011_tedysugiartaa``2101020011_tedysugiartaa`CREATE DATABASE lp;
+DROP DATABASE lp;
+USE lp;
+
+
+
+CREATE TABLE payment(
+id_payment VARCHAR(15),
+id_order VARCHAR(20),
+Metode VARCHAR(15),
+PRIMARY KEY(id_payment)
+);
